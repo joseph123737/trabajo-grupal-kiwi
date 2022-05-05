b1e946f3dcbbd8d259f91c79a3446744decfbed5
_cvee <cvelhninielkarti@gmail.com>
2022-05-05 11:19:41 +0200

    Creado template de '/' con botones y css básico

--- front/src/pages/home/HomePage.vue
+++ front/src/pages/home/HomePage.vue
@@ -1,35 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/img/logo.png" />
-    <h1>{{ info.app_name }}</h1>
-  </div>
+  <body class="home">
+    <ul>
+      <li>
+        <button>
+          LINEA CESTAS
+        </button>
+      </li>
+      <li>
+        <button>
+          LINEA CALIBRADO
+        </button>
+      </li>
+      <li>
+        <button>
+          CUADRE EXISTENCIAS
+        </button>
+      </li>
+    </ul>
+  </body>
 </template>
 
 <script>
 
 export default {
   name: 'Home',
-  data() {
-    return {
-      info: {}
-    }
-  },
-  mounted() {
-    this.loadData()
-  },
-  methods: {
-    async loadData() {
-      const response = await fetch('http://localhost:5000/api/info')
-      this.info = await response.json()
-    }
-  }
+  
 
 
 }
 </script>
 
 <style scoped>
-h1 {
-  font-style: italic;
+body {
+  height: 100%;
+  overflow-y: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+ul{
+  padding: 0;
+  margin: 0;
+}
+li{
+  list-style: none;
+  border: 1px solid black;
+  border-radius: 30px;
+  width: 50vw;
+  height: 25vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 3%;
+}
+button {
+  background: none;
+	color: inherit;
+	border: none;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
 }
 </style>
