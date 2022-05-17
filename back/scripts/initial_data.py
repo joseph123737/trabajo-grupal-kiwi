d6bd47d9604ab13f9325bc4e02a5ca0ed6b47200
_cvee <cvelhninielkarti@gmail.com>
2022-05-17 10:09:14 +0200

    BarCodeRepository creado con datos de prueba

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -1,15 +1,28 @@
+from datetime import date
+
+
 def main():
     import sys
 
     sys.path.insert(0, "")
 
     from src.domain.info import Info, InfoRepository
+    from src.domain.bar_code import BarCode, BarCodeRepository
+
 
     database_path = "data/database.db"
 
     info_repository = InfoRepository(database_path)
+    barcode_repository = BarCodeRepository(database_path)
 
     info_repository.save(Info(app_name="f5-seed-app"))
+
+    first_palot = BarCode(
+        lote_number= "K2100017001",
+        project= "L.CALIBRADO",
+        date= "2022-05-17"
+    )
+    barcode_repository.save_palot(first_palot)
 
 
 if __name__ == '__main__':
--- back/src/domain/bar_code.py
+++ back/src/domain/bar_code.py
@@ -1,12 +1,64 @@
+import sqlite3
+
 class BarCode:
-    def __init__(self, proyect, lote_number):
-        self.proyect = proyect
+    def __init__(self, project, lote_number, date):
+        self.project = project
         self.lote_number = lote_number
+        self.date = date
         self.user_name = "ik"
 
     def to_dict(self):
         return {
-            "proyect": self.proyect,
+            "project": self.project,
             "lote_number": self.lote_number,
+            "date": self.date,
             "user_name": self.user_name,
         }
+
+class BarCodeRepository:
+    def __init__(self, database_path):
+        self.database_path = database_path
+        self.init_tables()
+
+    def create_conn(self):
+        conn = sqlite3.connect(self.database_path)
+        conn.row_factory = sqlite3.Row
+        return conn
+
+    def init_tables(self):
+        sql = """
+            create table if not exists palots (
+                lote_number varchar primary key,
+                project varchar,
+                date varchar
+            )
+        """
+        conn = self.create_conn()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+
+    def get_palots(self):
+        sql = """select * from palots"""
+        conn = self.create_conn()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+
+        data = cursor.fetchall()
+        result = []
+        for item in data:
+            palot = BarCode(**item)
+            result.append(palot)
+        
+        return result
+       
+    def save_palot(self, palots):
+        sql = """insert into palots (lote_number, project, date) values (
+            :lote_number, :project, :date
+        ) """
+        conn = self.create_conn()
+        cursor = conn.cursor()
+        cursor.execute(sql,{"lote_number":palots.lote_number,"project":palots.project,"date":palots.date} )
+        conn.commit()
+
+  
