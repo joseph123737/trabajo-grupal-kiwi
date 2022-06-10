d5e8472b8203c771e16ab3eb8b98769bf445c6a1
rocio <rocio.gabriela.03c@gmail.com>
2022-06-10 09:23:11 +0200

    test barcode + refactor barcode (name)

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -7,8 +7,7 @@ def main():
     sys.path.insert(0, "")
 
     from src.domain.info import Info, InfoRepository
-    from src.domain.bar_code import BarCode, BarCodeRepository
-
+    from domain.barcode import BarCode, BarCodeRepository
 
     database_path = "data/database.db"
 
@@ -17,13 +16,9 @@ def main():
 
     info_repository.save(Info(app_name="f5-seed-app"))
 
-    first_palot = BarCode(
-        lote_number= "K2100017001",
-        project= "L.CALIBRADO",
-        date = ""
-    )
+    first_palot = BarCode(lote_number="K2100017001", project="L.CALIBRADO", date="")
     barcode_repository.save_palot(first_palot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- back/src/webserver.py
+++ back/src/webserver.py
@@ -1,7 +1,7 @@
 from datetime import date
 from flask import Flask, request
 from flask_cors import CORS
-from src.domain.bar_code import BarCode, BarCodeRepository
+from src.domain.barcode import BarCode, BarCodeRepository
 
 
 def create_app(repositories):
@@ -13,26 +13,26 @@ def create_app(repositories):
         return "...magic!"
 
     @app.route("/api/barcode", methods=["POST"])
-    def get_bar_code():
+    def get_barcode():
         body = request.json
         good_body = body["lote_number"].replace("'", "-")
-        bar_code_unconverted = BarCode(
+        barcode_unconverted = BarCode(
             project=body["project"],
             lote_number=good_body,
         )
-        erp_response = bar_code_unconverted.send_xml_to_erp()
+        erp_response = barcode_unconverted.send_xml_to_erp()
         dict_to_save = {
             "status_code": erp_response["status_code"],
-            "project": bar_code_unconverted.project,
-            "lote_number": bar_code_unconverted.lote_number,
+            "project": barcode_unconverted.project,
+            "lote_number": barcode_unconverted.lote_number,
             "response": erp_response["response"],
-            "date": bar_code_unconverted.date,
+            "date": barcode_unconverted.date,
         }
         repositories["barcode"].save_palot(dict_to_save)
         if erp_response["status_code"] == 200:
             return erp_response, 200
         if erp_response["status_code"] == 500:
-            bar_code_unconverted.send_error_to_erp(erp_response["response"])
+            barcode_unconverted.send_error_to_erp(erp_response["response"])
             if erp_response["response"].startswith("Bin"):
                 return erp_response, 409
             else:
